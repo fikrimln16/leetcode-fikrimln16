3997157dbf45dc919204f39349a6f43b69cd414a
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-05-20 17:46:33 +0700

    https://leetcode.com/submissions/detail/953798699/

--- buildArrayPermutation.js
+++ buildArrayPermutation.js
@@ -1,3 +1,21 @@
+/*
+Input: nums = [0,2,1,5,3,4]
+Output: [0,1,2,4,5,3]
+Explanation: The array ans is built as follows: 
+ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+    = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+    = [0,1,2,4,5,3]
+*/
+
+/*
+Input: nums = [5,0,1,2,3,4]
+Output: [4,5,0,1,2,3]
+Explanation: The array ans is built as follows:
+ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+    = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
+    = [4,5,0,1,2,3]
+*/
+
 const buildArray = (number) => {
   let array = [];
   number.forEach(element => {
