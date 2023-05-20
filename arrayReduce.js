079b61c952577bb8c2ec1f0dc0023ac5f7dadf83
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-05-20 17:44:51 +0700

    https://leetcode.com/submissions/detail/953830116/

--- arrayReduce.js
+++ arrayReduce.js
@@ -1,3 +1,43 @@
+/*
+Input: 
+nums = [1,2,3,4]
+fn = function sum(accum, curr) { return accum + curr; }
+init = 0
+Output: 10
+Explanation:
+initially, the value is init=0.
+(0) + nums[0] = 1
+(1) + nums[1] = 3
+(3) + nums[2] = 6
+(6) + nums[3] = 10
+The final answer is 10.
+*/
+
+/*
+Input: 
+nums = [1,2,3,4]
+fn = function sum(accum, curr) { return accum + curr * curr; }
+init = 100
+Output: 130
+Explanation:
+initially, the value is init=100.
+(100) + nums[0]^2 = 101
+(101) + nums[1]^2 = 105
+(105) + nums[2]^2 = 114
+(114) + nums[3]^2 = 130
+The final answer is 130.
+*/
+
+/*
+Input: 
+nums = []
+fn = function sum(accum, curr) { return 0; }
+init = 25
+Output: 25
+Explanation: For empty arrays, the answer is always init.
+*/
+
+
 var reduce = (nums, fn, init) => {
   //jika array kosong maka return initnya
   if (nums.length === 0) {
