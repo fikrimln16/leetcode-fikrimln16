89d15f353e210d3c27c700d5bc822dcaefd96ca1
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-05-20 17:52:01 +0700

    https://leetcode.com/submissions/detail/953803179/

--- functionComposition.js
+++ functionComposition.js
@@ -1,3 +1,31 @@
+/*
+Input: functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+Output: 65
+Explanation:
+Evaluating from right to left ...
+Starting with x = 4.
+2 * (4) = 8
+(8) * (8) = 64
+(64) + 1 = 65
+*/
+
+/*
+Input: functions = [x => 10 * x, x => 10 * x, x => 10 * x], x = 1
+Output: 1000
+Explanation:
+Evaluating from right to left ...
+10 * (1) = 10
+10 * (10) = 100
+10 * (100) = 1000
+*/
+
+/*
+Input: functions = [], x = 42
+Output: 42
+Explanation:
+The composition of zero functions is the identity function
+*/
+
 const compose = (functions) => { 
   return (value) => {
     return functions.reduceRight((result, fn) => {
