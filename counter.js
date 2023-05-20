442d2fc55a03ebf5245e64f26b3b722d00f01563
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-05-20 17:47:55 +0700

    https://leetcode.com/submissions/detail/949324359/

--- counter.js
+++ counter.js
@@ -1,3 +1,22 @@
+/*
+Input: 
+n = 10 
+["call","call","call"]
+Output: [10,11,12]
+Explanation: 
+counter() = 10 // The first time counter() is called, it returns n.
+counter() = 11 // Returns 1 more than the previous time.
+counter() = 12 // Returns 1 more than the previous time.
+*/
+
+/*
+Input: 
+n = -2
+["call","call","call","call","call"]
+Output: [-2,-1,0,1,2]
+Explanation: counter() initially returns -2. Then increases after each sebsequent call.
+*/
+
 /**
  * @param {number} n
  * @return {Function} counter
