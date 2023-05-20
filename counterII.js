f76c336651d86d72226e8328f7bc6601e0a39e93
Fikri Maulana <90541412+fikrimln16@users.noreply.github.com>
2023-05-20 17:49:19 +0700

    https://leetcode.com/submissions/detail/949330671/

--- counterII.js
+++ counterII.js
@@ -1,3 +1,25 @@
+/*
+Input: init = 5, calls = ["increment","reset","decrement"]
+Output: [6,5,4]
+Explanation:
+const counter = createCounter(5);
+counter.increment(); // 6
+counter.reset(); // 5
+counter.decrement(); // 4
+*/
+
+/*
+Input: init = 0, calls = ["increment","increment","decrement","reset","reset"]
+Output: [1,2,1,0,0]
+Explanation:
+const counter = createCounter(0);
+counter.increment(); // 1
+counter.increment(); // 2
+counter.decrement(); // 1
+counter.reset(); // 0
+counter.reset(); // 0
+*/
+
 /**
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
