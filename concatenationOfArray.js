4c3a216d4257987439613ef8b537e63ea24f84b7
Fikri Maulana <maulanafikri455@gmail.com>
2023-05-22 17:27:28 +0700

    https://leetcode.com/submissions/detail/955037918/

---
+++ concatenationOfArray.js
@@ -0,0 +1,37 @@
+/*
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+*/
+
+/*
+Input: nums = [1,3,2,1]
+Output: [1,3,2,1,1,3,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+- ans = [1,3,2,1,1,3,2,1]
+*/
+
+/*
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+*/
+
+var getConcatenation = (numRows) => {
+  
+  let array2 = []
+  numRows.forEach(element => {
+    array2.push(element)
+  });
+  
+  let result = numRows.concat(array2)
+  return result
+};
+
+console.log(getConcatenation([1,2,1]));
+console.log(getConcatenation([1,3,2,1]));
