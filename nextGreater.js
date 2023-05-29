0fcc101ab88dccd284aff6e88ee512f1630d4d52
Fikri Maulana <maulanafikri455@gmail.com>
2023-05-29 14:47:30 +0700

    https://leetcode.com/submissions/detail/959440350/

--- nextGreater.js
+++ nextGreater.js
@@ -31,6 +31,8 @@ var nextGreaterElement = function (nums1, nums2) {
     hashmap[nums2[i]] = i;
   }
 
+  hashmap
+
   for (let i = 0; i < nums1.length; i++) {
     const number = nums1[i];
     for (let j = hashmap[number]; j < nums2.length; j++) {
@@ -47,6 +49,6 @@ var nextGreaterElement = function (nums1, nums2) {
   return output;
 };
 
-// console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]));
+console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]));
 // console.log(nextGreaterElement([2,4], [1, 2, 3, 4]));
-console.log(nextGreaterElement([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]));
+// console.log(nextGreaterElement([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]));
